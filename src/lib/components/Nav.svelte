9b33a4c3791e1743c9ca401bfba436c2c895966c
ChristopherZinger <krzysztof.zinger@gmail.com>
2023-12-08 15:20:25 +0100

    create Nav component

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { goto } from '$app/navigation';
+	import OlympicCircles from '$lib/icons/OlympicCircles.svelte';
+</script>
+
+<nav class="flex justify-center py-5">
+	<button on:click={() => goto('/')}>
+		<OlympicCircles height="60" />
+	</button>
+</nav>
