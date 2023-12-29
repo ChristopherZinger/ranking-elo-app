7636b8619b75ae684bbbc79ce544e294a858a01e
ChristopherZinger <krzysztof.zinger@gmail.com>
2023-12-29 11:30:54 +0100

    create simple tournament game cell component

---
+++ src/routes/tournament/TournamentGameCell.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import type { Game } from '$lib/utils/tournament-utils';
+
+	export let game: Game;
+	export let isSelected: boolean;
+	export let onClick: () => void;
+
+	$: isSelectable = !game.results && game.players?.p1 && game.players?.p2;
+</script>
+
+<div
+	class:isSelected
+	class="game rounded-md overflow-hidden"
+	on:click={() => isSelectable && onClick()}
+	class:isSelectable
+>
+	<div class="px-2 bg-slate-50">{game.players?.p1.name || '-'}</div>
+	<div class="px-2 bg-slate-200">{game.players?.p2?.name || '-'}</div>
+</div>
+
+<style>
+	.game {
+		min-width: 75px;
+		box-sizing: border-box;
+	}
+
+	.isSelectable {
+		cursor: pointer;
+	}
+
+	.isSelected {
+		border: 1px solid;
+	}
+</style>
